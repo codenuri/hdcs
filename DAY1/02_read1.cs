687f480e0418194f555a8345e9fc1a1a4b23a2d7
ecourse <smkang@codenuri.co.kr>
2024-09-09 13:33:49 +0900

    Add files via upload

---
+++ DAY1/02_read1.cs
@@ -0,0 +1,18 @@
+using System;
+
+// Console 클래스 : 입출력 함수를 제공하는 클래스
+// Console.WriteLine : 출력
+// Console.ReadLine  : 입력
+
+// Console.ReadLine
+
+Console.Write("input your name >> ");
+
+string s = Console.ReadLine();
+
+Console.WriteLine($"입력한 문자열 : {s}");
+ 
+      
+  
+  
+   
