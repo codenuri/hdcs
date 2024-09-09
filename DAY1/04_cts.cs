127440cd6a3560c3cd4de2ed773f7cc7001097ee
ecourse <smkang@codenuri.co.kr>
2024-09-09 14:54:31 +0900

    Add files via upload

---
+++ DAY1/04_cts.cs
@@ -0,0 +1,20 @@
+using System;
+
+// CTS ( Common Type System )
+// => 타입의 정의는 언어가 아닌 CLR 에서 정의 한다.
+// => 각 언어의 타입은 CLR 타입의 별명이다.
+
+// 32비트의 정확한 타입이름은 : System.Int32 라는 구조체인데
+//                            일반적으로 "int" 라는 별명사용
+
+int   n1 = 0;
+Int32 n2 = 0;
+System.Int32 n3 = 0;
+
+double d1 = 0;
+Double d2 = 0;
+System.Double d3 = 0;
+
+string s1 = "A";
+String s2 = "A";
+System.String s3 = "A";
