bf11d984ff0b5b1771a05b1b05a536ece51c1efd
ecourse <smkang@codenuri.co.kr>
2024-09-10 13:41:55 +0900

    Add files via upload

---
+++ DAY2/06_parameter_modifier6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Point
+{
+    public int x = 0;
+    public int y = 0;
+}
+
+class Program
+{
+    public static void Foo(Point pt)
+    {
+        pt.x = 10; // !!
+    }
+    public static void Main()
+    {
+        Point p1 = new Point();
+        Foo(p1);
+    }
+}
