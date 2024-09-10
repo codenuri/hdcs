3c8498685769222cbc4cdc298b8ec9d09c44f288
ecourse <smkang@codenuri.co.kr>
2024-09-10 16:33:33 +0900

    Add files via upload

--- DAY2/12_inheritance2.cs
+++ DAY2/12_inheritance2.cs
@@ -16,9 +16,26 @@ class Program
     {
         Dog d = new Dog();
 
-        Dog    d1 = d; // ?
-        string s1 = d; // ?
-        Animal a1 = d; // ?
+        Dog    d1 = d; // ok.
+        
+//      string s1 = d; // error. C# 은 static type check 언어(타입에 엄격)
+                       // python 은 ok. 파이선은 모든것을 가리킬수있다.
+        
+        // #1. 기반 클래스 타입으로 참조 변수로 "파생 클래스 객체"를 가리킬수있다
+        // => upcating 이라고 합니다
+        // => 이유는 "메모리 그림"을 생각하면 됩니다.
+        Animal a1 = d; // 
+
+
+        // #2. Animal 타입인 a1 으로는 Animal 로 부터 물려받은 멤버만 접근가능
+        //     Dog 가 추가한 멤버는 접근 못함.
+        a1.age = 10;    // ok
+        a1.color = 20;  // error.
+
+        // #3. a1 으로 Dog 가 추가한 고유 멤버 접근하려면 캐스팅해야 합니다.
+        //     () 사용하면 됩니다.
+        //     => 컴파일러에게 a1이 가리키는 곳이 Dog 라고 알려주는 것
+        ((Dog)a1).color = 20; // ok
 
     }
 }
