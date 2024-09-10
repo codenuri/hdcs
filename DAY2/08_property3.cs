015ae701b5d86936d86d2c30fa2295fc9ca31096
ecourse <smkang@codenuri.co.kr>
2024-09-10 14:45:45 +0900

    Add files via upload

--- DAY2/08_property3.cs
+++ DAY2/08_property3.cs
@@ -5,8 +5,18 @@ class Person
 
     public Person(string n, int a)  { (name, age) = (n, a);  }
 
+    // 아래 같은 메소드를 "setter/getter" 라고 부릅니다.
     public void SetName(string n)   { name = n; }
     public string GetName()         { return name; }
+
+
+    // C#에는 "다른 언어 에는 없는" property 라는 문법이 있습니다.
+    // => Setter/Getter 를 쉽게 만드는 문법
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
 }
 
 class Program
@@ -17,5 +27,8 @@ class Program
 
         p1.SetName("lee");
         string s = p1.GetName();
+
+        p1.Name = "park";
+        s = p1.Name;
     }
 }
