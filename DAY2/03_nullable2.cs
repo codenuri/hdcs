da54a0f446a7856916e12d76c9625a0f1ab73d1b
ecourse <smkang@codenuri.co.kr>
2024-09-10 10:35:15 +0900

    Add files via upload

---
+++ DAY2/03_nullable2.cs
@@ -0,0 +1,13 @@
+﻿string s = null;
+int?   n = null;
+
+// 값이 있는지/없는지 조사하는 방법.
+// #1. null 과 비교
+if (s == null) { }
+if (n == null) { }
+
+// #2. hasValue 속성 조사
+// => Nullable<T> 만 가능
+// => reference type 은 없음.
+bool b1 = n.HasValue;
+//bool b2 = s.HasValue; // error
