742f6b935dc749f953d7683e473c304ebf24416d
ecourse <smkang@codenuri.co.kr>
2024-09-10 16:23:48 +0900

    Add files via upload

---
+++ DAY2/12_inheritance2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Animal
+{
+    public int age;
+}
+
+class Dog : Animal
+{
+    public int color;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Dog d = new Dog();
+
+        Dog    d1 = d; // ?
+        string s1 = d; // ?
+        Animal a1 = d; // ?
+
+    }
+}
