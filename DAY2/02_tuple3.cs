d91506d1103970f54138cde9949fb4898fdd372c
ecourse <smkang@codenuri.co.kr>
2024-09-10 10:18:52 +0900

    Add files via upload

--- DAY2/02_tuple3.cs
+++ DAY2/02_tuple3.cs
@@ -1,8 +1,13 @@
 ﻿// 인자로 받는 2개의 정수에 대해서
 // 합과, 곱을 반환하고 싶다!!
-int AddMul(int a, int b)
+// => 리턴값이 2개 필요 합니다.
+// => 다양한 방법이 있지만 가장 많이 사용하는 방법이 "tuple"
+(int, int) AddMul(int a, int b)
 {
-    return a + b;
+    return (a + b, a * b);
 }
 
-int ret = AddMul(3, 2);
+// #1. var 로 받기
+var ret = AddMul(3, 2);
+
+Console.WriteLine($"{ret.Item1}, {ret.Item2}");
