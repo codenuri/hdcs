9b28c09a7bc969b7b564b6644a32f81120b2bae4
ecourse <smkang@codenuri.co.kr>
2024-09-10 15:46:24 +0900

    Add files via upload

---
+++ DAY2/WPF2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;  
+
+class WPF1
+{
+    [STAThread] // 프로그램에서 GUI를 만들때는 반드시 적어야 합니다
+    public static void Main()
+    {
+        Window w = new Window();
+
+        w.Show();
+
+        MessageBox.Show("Hello, C#");
+    }
+}
