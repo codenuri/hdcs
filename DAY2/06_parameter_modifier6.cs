b2b141e6f547be25480558a2864a68753426176e
ecourse <smkang@codenuri.co.kr>
2024-09-10 13:46:18 +0900

    Add files via upload

--- DAY2/06_parameter_modifier6.cs
+++ DAY2/06_parameter_modifier6.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 
+// Point : class 이므로 reference type 
 class Point
 {
     public int x = 0;
     public int y = 0;
 }
-
 class Program
 {
+    // reference type 을 인자로 받는 경우는
+    // => ref 가 없어도
+    // => 원본 객체를 수정할수 있습니다.
+    // => "참조변수"에 대한 "복사본" 이므로 
+    // => "ref" 를 표기해도 됩니다.
     public static void Foo(Point pt)
     {
         pt.x = 10; // !!
@@ -16,5 +21,7 @@ class Program
     {
         Point p1 = new Point();
         Foo(p1);
+
+        Console.WriteLine($"{p1.x}"); // 10
     }
 }
