58ff696ca6eebafbe38f56fbcc16f5083914c7a0
ecourse <smkang@codenuri.co.kr>
2024-09-11 14:48:10 +0900

    Add files via upload

--- DAY3/06_interface3.cs
+++ DAY3/06_interface3.cs
@@ -9,19 +9,19 @@ class Program
         string s1 = "abcd";
         string s2 = s1; // s1, s2 는 같은 객체를 가리킴.
 
-        string s3 = s1.Clone(); // s1을 복제한 다른 객체를 생성해 달라.
+        string s3 = (string)s1.Clone(); // s1을 복제한 다른 객체를 생성해 달라.
 
 
-        // StringBuilder 도 참조 타입
-        StringBuilder sb1 = new StringBuilder("abcd");
-        StringBuilder sb2 = sb1; // sb1, sb2 같은 객체
-        StringBuilder sb3 = sb1.Clone();
+        // 배열 도 참조 타입
+        int[] x = { 1, 2, 3 };
+        int[] y = (int[])(x.Clone());
+
 
         // C# 사용시 "복사본 객체" 가 필요 하면
         // => Clone() 메소드 호출하면 됩니다.
         // => 그런데,!!!! "메소드 이름" 이 동일 함을 보장하기 위해
-        // => 인터페이스로 약속.(IClonable)
+        // => 인터페이스로 약속.(ICloneable)
 
-        IClonable c;
+        ICloneable c;
     }
 }
