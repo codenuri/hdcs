4fe9af052c03df2845d90e2ed9c30468774257f7
ecourse <smkang@codenuri.co.kr>
2024-09-11 11:37:37 +0900

    Add files via upload

---
+++ DAY3/04_equals3.cs
@@ -0,0 +1,42 @@
+using static System.Console;
+
+// 107 page
+
+class Point
+{
+    private int x = 0;
+    private int y = 0;
+    public Point(int a, int b) => (x, y) = (a, b);
+
+    // Object obj = new Point(); // 이 코드는 가능하지만
+    // int n = obj.x;      // 이렇게 접근은 안됩니다.
+    // int n = ((Point)obj.x)  이렇게 해야 합니다.
+    public override bool Equals(object? obj)
+    {
+        // 아래 코드는 "동일 객체" 여부가 아니라
+        // "객체의 상태" 끼리 조사하는 코드!
+
+        Point pt = (Point)obj;
+
+        return x == pt.x && y == pt.y;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Point p1 = new Point(1, 2);
+        Point p2 = p1;
+
+        Point p3 = new Point(1, 2);
+        Point p4 = new Point(1, 2);
+
+        bool b3 = p1.Equals(p2);
+        bool b4 = p3.Equals(p4);
+
+        WriteLine($"{b3}"); // True
+        WriteLine($"{b4}"); // True
+
+    }
+}
