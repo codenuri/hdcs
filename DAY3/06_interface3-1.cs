ab5c3a7368c80d6f61decc16ef40224319013a90
ecourse <smkang@codenuri.co.kr>
2024-09-11 14:51:18 +0900

    Add files via upload

---
+++ DAY3/06_interface3-1.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+class Program
+{
+    public static void Main()
+    {
+        // 변수의 크기 비교 방법
+
+        int n1 = 10;
+        int n2 = 20;
+        if (n1 < n2) { }
+
+
+        string s1 = "AB";
+        string s2 = "XY";
+        if (s1 < s2) { }
+    }
+}
