0dc024404449cf8023d2dc3a3d193801d93760a9
ecourse <smkang@codenuri.co.kr>
2024-09-11 16:21:07 +0900

    Add files via upload

--- DAY3/06_interface5.cs
+++ DAY3/06_interface5.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 
-class Point
+class Point : ICloneable
 {
     private int x = 0;
     private int y = 0;
     public Point(int a, int b) => (x, y) = (a, b);
+
+    public object Clone()
+    {
+        Point pt = new Point(x, y); // 자신과 동일한 객체 생성후
+        return pt;                  // 반환
+    }
 }
 
 class Program
@@ -14,6 +20,7 @@ class Program
         Point p1 = new Point(1, 2);
         Point p2 = p1;
 
+        Point p3 = p1.Clone(); // 만들어 봅시다.
     
     }
 }
