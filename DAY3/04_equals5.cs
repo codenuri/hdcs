90f6346af4e9b356ea08dfe1313781e2a901d92a
ecourse <smkang@codenuri.co.kr>
2024-09-11 13:12:59 +0900

    Add files via upload

---
+++ DAY3/04_equals5.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+// 107 page
+/*
+class Object
+{
+    public virtual bool Equals(Object   obj )
+    {
+
+    }
+}
+*/
+
+
+
+
+class Point
+{
+    private int x = 0;
+    private int y = 0;
+    public Point(int a, int b) => (x, y) = (a, b);
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(1, 2);
+
+        WriteLine($"{p1 == p2}");
+
+    }
+}
+
+
