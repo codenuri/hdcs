5b2326c4f1036fdfafb4a7d2d899a749ade08b6e
ecourse <smkang@codenuri.co.kr>
2024-09-11 15:30:48 +0900

    Add files via upload

--- DAY3/07_generic1.cs
+++ DAY3/07_generic1.cs
@@ -31,7 +31,15 @@ class Program
         int n1 = 10,  n2 = 20;
         double d1 = 1.1, d2 = 2.3;
 
-        swap<int>(ref n1, ref n2);		
-		swap<double>(ref d1, ref d2);
+        swap<int>(ref n1, ref n2);	// 1. swap(ref int, ref int) 버전의 함수 생성
+                                    // 2. 그리고, 이 위치는 call swap(int, int) 로 변경
+		swap<double>(ref d1, ref d2); // 1. swap(ref double, ref double) 버전함수 생성
+                                      // 2. call swap(doube) 버전
+
+        // 위 코드가 정확한 코드지만
+        // => 타입 인자를 생략해도 됩니다.
+        // => 타입 인자 생략시, 함수 인자를 보고 타입 추론됩니다.
+        swap(ref n1, ref n2);
+        swap(ref d1, ref d2);
     }
 }
