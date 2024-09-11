b76a6d301a02062b160ed55420f97dcbf17a2e02
ecourse <smkang@codenuri.co.kr>
2024-09-11 11:15:19 +0900

    Add files via upload

---
+++ DAY3/03_to_string3.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+class Program
+{
+    public static void Main()
+    {
+        List<int> s = new List<int>();
+
+        s.Add(1);
+        s.Add(2);
+        s.Add(3);
+
+        Console.WriteLine(s.ToString());
+    }
+}
