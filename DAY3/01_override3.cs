6919b1f8fa59de0e4a87ff6951fdaadbee8beb12
ecourse <smkang@codenuri.co.kr>
2024-09-11 09:27:57 +0900

    Add files via upload

---
+++ DAY3/01_override3.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+class Animal
+{
+    public void Cry1() { WriteLine("1. Animal Cry1"); }
+
+    public virtual void Cry2() { WriteLine("1. Animal Cry2"); }
+}
+
+
+
+
+class Dog : Animal
+{
+    public new void Cry1() { WriteLine("2. Dog Cry1"); }
+}
+class Program
+{
+    public static void Main()
+    {
+        Animal a = new Animal();
+        Dog d = new Dog();
+
+        Animal a2 = d;
+
+        a2.Cry1();  
+    }
+}
+
