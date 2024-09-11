07d2a56cf928e776e8ed2fe2c5c1e9ac1cf09ce9
ecourse <smkang@codenuri.co.kr>
2024-09-11 16:17:58 +0900

    Add files via upload

---
+++ DAY3/06_interface5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Point
+{
+    private int x = 0;
+    private int y = 0;
+    public Point(int a, int b) => (x, y) = (a, b);
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Point p1 = new Point(1, 2);
+        Point p2 = p1;
+
+    
+    }
+}
