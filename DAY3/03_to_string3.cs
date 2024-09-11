045bf1c3d4fb004835463e538962158ceb8282eb
ecourse <smkang@codenuri.co.kr>
2024-09-11 11:20:03 +0900

    Add files via upload

--- DAY3/03_to_string3.cs
+++ DAY3/03_to_string3.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System.Collections;
+using static System.Console;
 
 class Program
 {
@@ -10,6 +11,18 @@ class Program
         s.Add(2);
         s.Add(3);
 
-        Console.WriteLine(s.ToString());
+        // s.ToString() : s객체의 상태 정보를 문자열로 반환
+        //          1. s의 클래스 이름을 반환
+        //          2. [1,2,3] 이라는 데이타를 알고 싶다. 
+        //          => 보통 2번을 원하게 됩니다.
+        //          => 그렇게 동작하려면 List 를 만들때 "ToString()" 을
+        //              override 해야 하는데.
+        //              하지 않아서, 아래 코드는 클래스 이름 출력. 
+        
+        WriteLine( s.ToString() );
+
+        
+
     }
+    
 }
