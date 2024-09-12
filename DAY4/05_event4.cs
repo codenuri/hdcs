8785aca1b83e298f88daa1aa7fe889e87e7ad21d
ecourse <smkang@codenuri.co.kr>
2024-09-12 13:19:24 +0900

    Add files via upload

--- DAY4/05_event4.cs
+++ DAY4/05_event4.cs
@@ -2,10 +2,15 @@ using static System.Console;
 
 
 delegate void HANDLER();
+// class HANDLER {}
 
 class Button
 {
+    public int   x; // 
+    //     타입  변수이름
+
 //  public HANDLER Click = null;        // +=, -=, = 모두 사용가능
+//         타입    변수이름
 
     public event HANDLER Click = null;  // +=, -= 만 사용가능.
                                         // = 는 사용할수 없음.
@@ -25,9 +30,10 @@ class Program
     {
         Button btn1 = new Button();
 
-        btn1.Click = F1;
+        btn1.Click += F1;
         btn1.Click += F2;
-        btn1.Click = F3; 
+//      btn1.Click = F3;  // error
+        btn1.Click += F3;  // ok
 
         btn1.UserPressButton();
     }
