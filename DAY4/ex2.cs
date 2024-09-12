3a471561d2589e7f8eb98a4a4e0a949e2ad71cac
ecourse <smkang@codenuri.co.kr>
2024-09-12 14:39:27 +0900

    Add files via upload

---
+++ DAY4/ex2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+// step2. windows 만들기
+// => System.Windows.Window  라는 클래스 사용
+
+class Program
+{
+    public static void Main()
+    {
+        Window w = new Window();
+        w.Title = "Hello, WPF";
+        w.Show();
+
+        MessageBox.Show("Hello, WPF");
+    }
+}
