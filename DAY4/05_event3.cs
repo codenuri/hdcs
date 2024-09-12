1b81bab3fc636a35f5aa88f7cd7d5f42a143a28f
ecourse <smkang@codenuri.co.kr>
2024-09-12 13:14:32 +0900

    Add files via upload

--- DAY4/05_event3.cs
+++ DAY4/05_event3.cs
@@ -24,7 +24,12 @@ class Program
 
         btn1.Click = F1;
         btn1.Click += F2;
-        btn1.Click += F3;
+//      btn1.Click += F3;
+
+        btn1.Click = F3; // += 로 하려고 했는데, 실수로 = 를 사용했다
+                        // 이전 등록된 메소드가 모두 reset 되었다!!
+                        // 이런 실수를 막기위해서 나온 키워드가 "event"
+                        // => 다음소스에서
 
         btn1.UserPressButton();
     }
