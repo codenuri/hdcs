f6f534438d0f5912fdec2c4ffe55bfbc7e6b730b
ecourse <smkang@codenuri.co.kr>
2024-09-12 13:21:06 +0900

    Add files via upload

---
+++ DAY4/05_event5.cs
@@ -0,0 +1,12 @@
+using static System.Console;
+
+class Program
+{
+    public static void F0()        => WriteLine("F0");
+    public static void F1(int arg) => WriteLine("F1");
+    public static void F2(int arg1, int arg2) => WriteLine("F2");
+
+    public static void Main()
+    {
+    }
+}
