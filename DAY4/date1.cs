ae1893761edd92de0be79163e404f482c40c88a8
ecourse <smkang@codenuri.co.kr>
2024-09-12 10:08:42 +0900

    Add files via upload

--- DAY4/date1.cs
+++ DAY4/date1.cs
@@ -5,16 +5,27 @@ using static System.Console;
 // => Date 라는 타입을 만들자
 class Date
 {
-    ? int year;
-    ? int month;
-    ? int day;
-}
+    private int year;
+    private int month;
+    private int day;
 
+    public Date(int y, int m, int d) => (year, month, day) = (y, m, d);
+
+    // private 필드에서 대한 속성 제공
+    public int Month
+    {
+        get { return month; }
+        set { if (value >= 1 && value <= 12) month = value; }
+    }
+}
 
 class Program
 {
     public static void Main()
     {
-
+        Date d1 = new Date(2024, 9, 12);
+        d1.Month = 10;
+        WriteLine($"{d1.Month}");
     }
 }
+
