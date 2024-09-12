f65926cbb780938c948c3aef628f718d7cb7b99b
ecourse <smkang@codenuri.co.kr>
2024-09-12 10:48:23 +0900

    Add files via upload

---
+++ DAY4/02_static6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// 이미 C# 에는 "Math" 클래스가 있습니다.
+// => 모든 메소드가 "static method" 입니다.
+
+using static System.Math; // Math 클래스 안에 있는
+                          // 모든 static method 를 
+                          // Math 라는 클래스 이름 없이 사용하겠다.
+
+class Program
+{
+    public static void Main()
+    {
+        double d1 = Math.Sqrt(3);
+        double d2 = Math.Cos(3.4);
+
+        double d3 = Cos(3.4);
+    }
+}
+/*
+namspace AA
+{
+    class K { }
+    namespace BB
+    {
+    class K { }
+    }
+}
+*/
