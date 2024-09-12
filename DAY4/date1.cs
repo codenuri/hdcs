462a7f2aeb98111cc690b71747b34785f3982ec5
ecourse <smkang@codenuri.co.kr>
2024-09-12 10:04:13 +0900

    Add files via upload

---
+++ DAY4/date1.cs
@@ -0,0 +1,20 @@
+﻿// date1.cs
+using static System.Console;
+
+// 프로그램에서 날짜를 다루는 경우가 많다.
+// => Date 라는 타입을 만들자
+class Date
+{
+    ? int year;
+    ? int month;
+    ? int day;
+}
+
+
+class Program
+{
+    public static void Main()
+    {
+
+    }
+}
