caad49832c3a2cb786e96db9bced95a644b234a1
ecourse <smkang@codenuri.co.kr>
2024-09-12 15:22:42 +0900

    Add files via upload

---
+++ DAY4/ex5-2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+
+// step5-2. App Event 는 App 클래스에서!!
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+    }
+}
+
+class App : Application
+{
+    public App()
+    {
+        this.Startup += App_Startup;
+    }
+    private void App_Startup(object sender, StartupEventArgs e)
+    {
+        Console.WriteLine("프로그램시작시 해야할일수행");
+    }
+}
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application app = new Application();
+        MainWindow w = new MainWindow();
+        w.Show();
+        app.Run();
+    }
+}
