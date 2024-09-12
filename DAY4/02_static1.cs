3cedd7e4120c26159f5b7a730db7b6666864a128
ecourse <smkang@codenuri.co.kr>
2024-09-12 09:25:45 +0900

    Add files via upload

---
+++ DAY4/02_static1.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+// Car 객체를 몇개나 생성했는지 갯수를 파악하고 싶다.
+
+class Car
+{
+    private int color = 0;
+    private int speed = 0;
+
+    public int cnt = 0;
+
+    public Car() { ++cnt; }
+
+}
+class Program
+{
+    public static void Main()
+    {
+        Car c1 = new Car();
+        Car c2 = new Car();
+
+        Console.WriteLine($"{c1.cnt}"); // ??
+    }
+}
