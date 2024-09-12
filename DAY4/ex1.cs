fa360b8815057fdae938a5f61b23b9309a74a77c
ecourse <smkang@codenuri.co.kr>
2024-09-12 14:31:12 +0900

    Add files via upload

---
+++ DAY4/ex1.cs
@@ -0,0 +1,18 @@
+﻿// ex1.cs
+using System;
+using System.Windows;
+
+// WPF 
+// => Windows Presentable Framework
+// => .net 기반(C#) GUI 라이브러리
+// => WinForm 의 다음 버전(대체자)
+
+// step1. 환경설정
+
+class Program
+{
+    public static void Main()
+    {
+        MessageBox.Show("Hello, WPF");
+    }
+}
