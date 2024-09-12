cfb9d41afccbabb032927d9ba0ad6b88f143b34e
ecourse <smkang@codenuri.co.kr>
2024-09-12 16:24:09 +0900

    Add files via upload

--- DAY4/ex9-1.cs
+++ DAY4/ex9-1.cs
@@ -14,9 +14,15 @@ class MainWindow : Window
         this.Content = sp;
 
         // #2. 자식 컨트롤은 모두 StackPanel(Layout)에 추가
+        // sp.Children : List 입니다. 따라서, Add 로 컨트롤 추가
+
         sp.Children.Add(new Button { Content = "btn1" });
         sp.Children.Add(new Button { Content = "btn2" });
         sp.Children.Add(new Button { Content = "btn3" });
+
+        // #3. stack panel 은 자식들을 한줄로 배치
+        // => 다양한 속성도 있습니다.
+        sp.Orientation = Orientation.Horizontal;
     }
 }
 
