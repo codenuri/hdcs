fec92d7646a4c6d7e3be81d7e34aa6b25ecebee3
ecourse <smkang@codenuri.co.kr>
2024-09-12 14:36:36 +0900

    Add files via upload

--- DAY4/ex1.cs
+++ DAY4/ex1.cs
@@ -8,6 +8,10 @@ using System.Windows;
 // => WinForm 의 다음 버전(대체자)
 
 // step1. 환경설정
+// 1. 콘솔 프로젝트로 만들고
+// 2. 프로젝트 파일에서 1개 항목 추가, 1개 항목 수정
+// 	  추가할것 : <UseWPF>true</UseWPF> 
+//    변경할것 : <TargetFramework>net8.0-windows</TargetFramework>
 
 class Program
 {
