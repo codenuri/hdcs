e28d5179c4448a603f4c19470b670fb5be2d3c84
ecourse <smkang@codenuri.co.kr>
2024-09-12 09:42:44 +0900

    Add files via upload

--- DAY4/02_static3.cs
+++ DAY4/02_static3.cs
@@ -11,18 +11,28 @@ class Car
 
     public Car() { ++cnt; }
 
-    public int GetCount() {  return cnt; }
+//  public int GetCount() {  return cnt; }      // instance method 
+                                                // 객체가 있어야만 호출 가능하다.
+                                                // 객체와 관련있는 메소드!!
 
+    public static int GetCount() { return cnt; }    // static method
+                                                    // 객체 없이 호출 가능.
+                                                    // 특정 객체(자동차)와는 관련없는
+                                                    // 기능 수행
+                                                    // "Car.GetCount()" 로 호출
 }
 class Program
 {
     public static void Main()
     {
+        // 이 위치에서 자동차의 갯수를 알고 싶다.
+        Console.WriteLine($"{Car.GetCount()}");
+
         Car c1 = new Car();
         Car c2 = new Car();
 
 //      Car.cnt = 100; // 이렇게 사용할수 없도록 private 으로
 
-        Console.WriteLine($"{c1.GetCount()}"); 
+        Console.WriteLine($"{Car.GetCount()}"); 
     }
 }
