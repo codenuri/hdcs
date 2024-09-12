31a45ad50b8c5931613e5d9696b83e3174189098
ecourse <smkang@codenuri.co.kr>
2024-09-12 09:22:15 +0900

    Add files via upload

--- DAY4/01_this2.cs
+++ DAY4/01_this2.cs
@@ -13,9 +13,12 @@ class People
     }
 
     // this 활용 #2. this 를 반환하는 메소드.!
+    // => 메소드 호출을 연속적으로 할수 있습니다.
+    // => Java 에서 아주 널리사용. Java 진영에서는 "빌더"라는 이름을 가진 기법 
+    // => C# 은 이렇게 하지 않고, 주로, Property 사용
     public People SetName(string name)
     {
-        name = name;
+        this.name = name;
         return this;
     }
 }
@@ -26,7 +29,11 @@ class Program
     {
         People p = new People();
 
-        p.SetName("kim").SetAge(20);
+        p.SetName("kim").SetAge(20); // java style
+
+        // C# 은 아래 처럼(지금은 속성 안만들었으므로 에러)
+//      p.Name = "kim";
+//      p.Age = 20;
 
     }
 }
