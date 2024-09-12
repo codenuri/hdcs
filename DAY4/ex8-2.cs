7b0e87641345e5f11c8565bcc2fc6a83f42ebb51
ecourse <smkang@codenuri.co.kr>
2024-09-12 16:13:24 +0900

    Add files via upload

--- DAY4/ex8-2.cs
+++ DAY4/ex8-2.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 // ex8-2. Contents 속성 - 중요
 // => WPF 의 대부분의 GUI 는 한개의 객체를 표현(표시, display)할수 있다.
@@ -14,9 +15,27 @@ class MainWindow : Window
         // this.Content = "Hello";
 
         // #2. 자식윈도우 버튼
+        /*
         Button btn = new Button();
-
+        btn.Content = "확인";
         this.Content = btn;
+        */
+        // 위 3줄은 아래 한줄로 가능
+        // => 객체 생성시 {} 하고, 속성을 바로 지정.
+        // this.Content = new Button { Content = "확인" };
+
+        // #3. 그림연결
+        // => Contents 에 연결되려면 UIElement 라는 클래스로 부터 파생된
+        //    클래스만 가능
+        Uri uri = new Uri("D:\\totoro.jpg");
+
+        BitmapImage bitmap = new BitmapImage(uri);
+
+        //this.Content = bitmap; // error
+
+        Image img = new Image { Source = bitmap };
+
+        this.Content = img;
     }
 }
 
