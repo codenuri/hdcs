a7f1d2143cfbb65f142cf508de2fa1f577b8e531
ecourse <smkang@codenuri.co.kr>
2024-09-12 10:19:34 +0900

    Add files via upload

--- DAY4/date2.cs
+++ DAY4/date2.cs
@@ -7,6 +7,13 @@ class Date
     private int month;
     private int day;
 
+    // 월별 날짜 관리(복습시, 아래 날짜수 맞게 작성해 보세요)
+    // => 월별 날짜수는 "객체당 한개" 일 필요 없습니다
+    // => 날짜 객체가 10개 라도 아래 데이타는 한개면 됩니다.
+    // => 핵심 : 아래 데이타는 "static 필드" 이어야 합니다.
+    private static int[] days = { 31, 30, 28, 30, 30, 30, 30, 30, 30, 30, 30, 30 };
+
+
     public Date(int y, int m, int d) => (year, month, day) = (y, m, d);
 
     public int Month
@@ -15,9 +22,11 @@ class Date
         set { if (value >= 1 && value <= 12) month = value; }
     }
     
-    Date AfterDays(int ds)
+    public Date AfterDays(int ds)
     {
         Date d = new Date(year, month, day + ds);
+                            // => 현재는 잘못된 구현
+                            // => 복습할때 제대로 구현해 보세요
         return d;
     }
 }
