c5c758e9a327ea2fa949cc7c6196d1d3f63b4151
ecourse <smkang@codenuri.co.kr>
2024-09-12 10:39:25 +0900

    Add files via upload

---
+++ DAY4/02_static4.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+// 다음중 에러를 모두 고르세요
+class Car
+{
+    private int color = 0;
+    private static int cnt = 0;
+
+    public Car M1()     
+    {
+        color = 0;      // 1
+        cnt = 0;        // 2
+        return this;    // 3
+    }
+    public static Car M2()
+    {
+        color = 0;      // 1
+        cnt = 0;        // 2
+        return this;    // 3
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Car.M2();
+    }
+}
