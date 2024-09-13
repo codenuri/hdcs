21198d93aa0368feb5754cb57773fef56ee1bdfd
ecourse <smkang@codenuri.co.kr>
2024-09-13 15:49:02 +0900

    Add files via upload

---
+++ DAY5/05_thread7.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+
+    public static Task<int> SumAsync(int first, int last)
+    {
+        return Task.Run(() =>
+        {
+            int s = 0;
+            for (int i = first; i <= last; i++)
+            {
+                s += i;
+                Thread.Sleep(10);
+            }
+            return s;
+        });
+    }
+
+    public static async void Foo()
+    {
+        //        Task<int> t = SumAsync(1, 1000);
+
+        Console.WriteLine($"Foo #1 : {Thread.CurrentThread.ManagedThreadId}");
+
+        int ret = await SumAsync(1, 1000);
+
+        Console.WriteLine($"Foo #2 : {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    public static void Main()
+    {
+        Foo();
+        Console.WriteLine("Main 계속실행");
+
+        Console.ReadLine();
+
+    }
+
+}
