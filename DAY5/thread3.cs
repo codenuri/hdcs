2912a03ea1d7f2b3452af90cde9969afc8b08cfe
ecourse <smkang@codenuri.co.kr>
2024-09-13 14:52:30 +0900

    Add files via upload

---
+++ DAY5/thread3.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static int Sum(int first, int last)
+    {
+        int s = 0;
+        for (int i = first; i <= last; i++)
+        {
+            s += i;
+            Thread.Sleep(10);
+        }
+        return s;
+    }
+
+    public static void Main()
+    {
+        Thread t = new Thread(() => Sum(1, 2));
+        t.Start();
+
+    }
+
+}
