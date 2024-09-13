7f0f56f8a3e53f62b0ead6628bd45b7116a85716
ecourse <smkang@codenuri.co.kr>
2024-09-13 15:00:35 +0900

    Add files via upload

---
+++ DAY5/04_find.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+class Program
+{
+    public static bool Foo(int n) { return n % 2 == 0; }
+
+    public static void Main()
+    {
+        int[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        // x.First(함수) : x의 모든 요소를 차례대로 함수에 보내서 
+        //                처음으로 0을 반환하는 것을 찾고 싶다.
+
+        int ret = x.First(Foo);
+
+        Console.WriteLine(ret);
+    }
+}
---
+++ DAY5/05_thread1.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+class Program
+{
+    public static void F1() {  }
+    public static void F2(object arg) { }
+    public static void F3(int a, int b) { }
+
+    public static void Main()
+    {
+        Thread t1 = new Thread(F1);
+        t1.Start();
+
+    }
+}
