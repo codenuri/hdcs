5261e1377af5a55adcc850d4348ca97787a367d7
ecourse <smkang@codenuri.co.kr>
2024-09-13 14:05:57 +0900

    Add files via upload

---
+++ DAY5/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlidingPuzzle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int COUNT = 5;
+        private const int EMPTY = COUNT * COUNT - 1;
+
+        public void InitGrid()
+        {
+            for( int i = 0; i < COUNT; i++ )
+            {
+                gameGrid.RowDefinitions.Add(new RowDefinition());
+                gameGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
