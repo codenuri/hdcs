3cf4d190cee2119e65f005b6c4769f6dd668af5f
ecourse <smkang@codenuri.co.kr>
2024-09-13 15:13:03 +0900

    Add files via upload

--- DAY5/05_thread1.cs
+++ DAY5/05_thread1.cs
@@ -1,5 +1,7 @@
 ﻿using static System.Console;
 
+// 05_thread1.cs
+
 class Program
 {
     public static void F1() {  }
@@ -8,8 +10,16 @@ class Program
 
     public static void Main()
     {
+        // #1. 스레드 생성하는 방법
         Thread t1 = new Thread(F1);
         t1.Start();
+
+        Thread t2 = new Thread(F2);
+        t2.Start("Hello");  // 인자가 한개 있는 경우. 
+
+        // #2. 인자가 여러개라면 람다 표현식으로
+        Thread t3 = new Thread(() => F3(1, 2) );
+        t3.Start();
 
     }
 }
