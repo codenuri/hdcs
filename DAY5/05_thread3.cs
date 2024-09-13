b1705cfd4c5c6fac5d6c32e118abcfad698cbe0e
ecourse <smkang@codenuri.co.kr>
2024-09-13 15:28:23 +0900

    Add files via upload

---
+++ DAY5/05_thread3.cs
@@ -0,0 +1,37 @@
+﻿// git thread3.cs
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static int Sum(int first, int last)
+    {
+        int s = 0;
+        for (int i = first; i <= last; i++)
+        {
+            s += i;
+            Thread.Sleep(10);
+        }
+        return s;
+    }
+
+    public static void Main()
+    {
+//      Sum(1, 1000);  // 메소드 종료를 대기 
+
+        Thread t = new Thread(() => Sum(1, 1000));
+        t.Start();  // 새로운 스레드가 실행하므로
+                    // 주스레드는 계속 실행됨.
+                        
+                    // 그런데, Sum 의 결과를 얻을수가 없습니다.
+                    // => 얻으려면 복잡한 코드를 직접 작성해야 한다.
+
+        Console.WriteLine("Main 계속실행");
+
+
+
+
+    }
+
+}
---
+++ DAY5/05_thread4.cs
@@ -0,0 +1,34 @@
+﻿// git thread3.cs
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static int Sum(int first, int last)
+    {
+        int s = 0;
+        for (int i = first; i <= last; i++)
+        {
+            s += i;
+            Thread.Sleep(10);
+        }
+        return s;
+    }
+
+    public static void Main()
+    {
+        // Task 클래스
+        // => 스레드를 생성하고 결과까지 얻을수 있도록 설계된 클래스
+        // => Thread 클래스를 직접 사용하는 것 보다 편리하다.
+
+        Task<int> t = Task.Run(() => Sum(1, 1000));
+
+        Console.WriteLine("Main 계속실행");
+
+        // 주스레드는 계속 실행하다가
+        // 결과 필요하면 아래 처럼 얻으면 됩니다.
+        int ret = t.Result; // 아직 연산이 안끝난경우는 대기.
+    }
+
+}
