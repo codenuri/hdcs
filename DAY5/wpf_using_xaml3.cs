e8059657f22cd4f8b32ca2ae4086e3ec68e139d6
ecourse <smkang@codenuri.co.kr>
2024-09-13 09:44:49 +0900

    Add files via upload

---
+++ DAY5/wpf_using_xaml3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+
+class MainWindow : Window
+{
+    public void InitializeComponent()
+    {
+
+    }
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        //....
+    }
+}
+
+
+
+class App : Application
+{
+    public App()
+    {
+    }
+
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+
+
+
+        FileStream fs = new FileStream("../../../MyXaml3.xaml", FileMode.Open);
+
+        Window w = (Window)XamlReader.Load(fs);
+
+        fs.Dispose();
+
+
+
+
+        w.Show();
+        app.Run();
+    }
+}
+
+
+// http://schemas.microsoft.com/winfx/2006/xaml/presentation
