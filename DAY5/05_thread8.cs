38cd88b503d56b83fe2057095832db46fbe4d9f3
ecourse <smkang@codenuri.co.kr>
2024-09-13 15:53:19 +0900

    Add files via upload

---
+++ DAY5/05_thread8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+
+    public static async void Foo()
+    {
+
+        FileStream fs = new FileStream("a.txt", FileMode.Create);
+
+        
+            byte[] buff = new byte[1024 * 1024 * 1000]; // 1G
+                
+            // fs.Write(buff, 0, buff.Length); // 동기
+
+            await fs.WriteAsync(buff);
+
+            Console.WriteLine("WriteAsync Finish");
+
+    }
+
+    public static void Main()
+    {
+        Foo();
+        Console.WriteLine("Main 계속실행");
+
+        Console.ReadLine();
+
+    }
+
+}
